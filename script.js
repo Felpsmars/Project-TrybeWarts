ab3470ed875870d3fd22a24449c453252d0eb41a
igor.marinho <igormarinhoargollo@gmail.com>
2021-07-21 19:06:09 -0300

    Trybewarts Project - basicos completos

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 const query = document.querySelector.bind(document);
 // const queryAll = document.querySelectorAll.bind(document);
 
-const button = query(".button");
-const email = query(".email");
-const password = query(".password");
+const button = query('.button');
+const email = query('.email');
+const password = query('.password');
 
 function verify() {
-  if (email.value === "tryber@teste.com" && password.value === "123456") {
-    alert("Olá, Tryber!");
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Login ou senha inválidos.");
+    alert('Login ou senha inválidos.');
   }
 }
 
-button.addEventListener("click", verify);
+button.addEventListener('click', verify);
 
-let checkBox = query("#checkbox");
-let submit = query("#submit-btn");
+const checkBox = query('#checkbox');
+const submit = query('#submit-btn');
 
 function checkChange() {
-  if (checkBox.value === "") {
+  if (checkBox.value === '') {
     submit.disabled = true;
   } else {
     submit.disabled = false;
   }
 }
 
-checkBox.addEventListener("click", checkChange);
+checkBox.addEventListener('click', checkChange);
